ed3a83593cff5ab66ea854eea4d9f5d2e47fe799
dongyuri <dongyuri@gmail.com>
2026-03-25 11:05:33 +0900

    feat: Path & Query Parameter 추가

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello, FastAPI!"}
 
+# Path Parameter
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+# Query Parameter
+@app.get("/search")
+def search_item(q: str, limit: int=10):
+    return {"query": q, "limit": limit}
+
