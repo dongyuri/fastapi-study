50904c2769345ca096171fa567f9ed536b4bd107
dongyuri <dongyuri@gmail.com>
2026-03-25 13:47:34 +0900

    feat: 의존성 주입 Depends 추가

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 
 app = FastAPI(
@@ -21,17 +21,23 @@ class ItemResponse(BaseModel):
     price: float
     is_available: bool
 
+# 공통 쿼리 파라미터 의존성
+def common_params(skip: int=0, limit: int=10):
+    return {"skip": skip, "limit": limit}
+
 @app.get("/", tags=["root"])
 def read_root():
     return {"message": "Hello, FastAPI!"}
 
-# Path Parameter
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
 
+@app.get("/items", tags=["items"])
+def read_items(commons: dict = Depends(common_params)):
+    return {"params": commons}
 
-# POST 요청
+@app.get("/users", tags=["users"])
+def read_users(commons: dict = Depends(common_params)):
+    return {"params": commons}
+
 @app.post("/items", response_model=ItemResponse, tags=["items"])
 def create_item(item: ItemCreate):
     return item
