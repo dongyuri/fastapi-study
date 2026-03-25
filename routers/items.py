3225e90fd98f3075d1f6adc3753fbc81d972d70f
dongyuri <dongyuri@gmail.com>
2026-03-25 14:16:59 +0900

    feat: HTTPException 에러 추가 처리

--- routers/items.py
+++ routers/items.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 
 router = APIRouter(
@@ -17,6 +17,13 @@ class ItemResponse(BaseModel):
     price: float
     is_available: bool
 
+
+# 임시 데이터
+fake_db = {
+    1: {"name": "커피", "price": 4500.0, "is_available": True},
+    2: {"name": "녹차", "price": 3000.0, "is_available": False}
+}
+
 def common_params(skip: int = 0, limit: int = 10):
     return {"skip": skip, "limit": limit}
 
@@ -24,6 +31,15 @@ def common_params(skip: int = 0, limit: int = 10):
 def read_items(commons: dict = Depends(common_params)):
     return {"params": commons}
 
+@router.get("/{item_id}")
+def read_item(item_id: int):
+    if item_id not in fake_db:
+        raise HTTPException(status_code=404, detail="아이템을 찾을수 없습니다")
+    return fake_db[item_id]
+
 @router.post("", response_model=ItemResponse)
 def create_item(item: ItemCreate):
+    if item.price <= 0 :
+        raise HTTPException(status_code=404, detail="가격은 0보다 커야 합니다")
     return item
+
